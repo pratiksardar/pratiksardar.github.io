a40f12bfff7f2155f1ea3c3527a3f2111278df87
Praitk Sardar <pratiksardar@gmail.com>
2021-10-02 12:24:28 -0400

    WIP milestone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <BrowserRouter>
+          <div>
+            <nav>
+              <ul id="navigation">
+                {/* <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li> */}
+              </ul>
+            </nav>
+          </div>
+          
+            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+          </BrowserRouter>
+          </div>
+            );
+  }
 }
 
 export default App;
