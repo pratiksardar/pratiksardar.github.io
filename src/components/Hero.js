22c7940de96418a7a1daba30fd44f80a92e4e237
Praitk Sardar <pratiksardar@gmail.com>
2021-10-03 09:01:58 -0400

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,10 @@ const Hero = () => {
     return (
         <div className="bg-white h-screen flex flex-center justify-center items-center">
             <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>
-                Hello...
+                Hello... 
+                
                 </h1>
+                <p>🐱</p>
         </div>
     )
 }
