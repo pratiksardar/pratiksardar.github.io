315a16187730028b823930787a1e49621bd749b1
Praitk Sardar <pratiksardar@gmail.com>
2021-10-03 08:43:58 -0400

    changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Bounce from './Bounce'
 
 const Hero = () => {
     return (
         <div className="bg-white h-screen flex flex-center justify-center items-center">
             <h1 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>
-                Hello !!!
+                Hello...
                 </h1>
         </div>
     )
